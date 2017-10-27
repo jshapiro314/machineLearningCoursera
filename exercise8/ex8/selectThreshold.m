2f290446d6531914950716e22952e826c0c4e48d
jshapiro314 <jshapiro314@users.noreply.github.com>
2017-10-26 22:01:49 -0400

    implemented code to select epsilon threshold for anomaly detection

--- exercise8/ex8/selectThreshold.m
+++ exercise8/ex8/selectThreshold.m
@@ -24,9 +24,16 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
+    crossValPred = (pval < epsilon);
+    
+    fp = sum((crossValPred == 1) & (yval == 0));
+    tp = sum((crossValPred == 1) & (yval == 1));
+    fn = sum((crossValPred == 0) & (yval == 1));
 
+    prec = tp / (tp + fp);
+    recall = tp / (tp + fn);
 
-
+    F1 = (2 * prec * recall) / (prec + recall);
 
 
 
